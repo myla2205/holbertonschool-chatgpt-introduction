c8dedbcd309d937baddd15810fb4d42b986492f1
Kamyl <myla2205@gmail.com>
2024-09-03 11:23:27 -0400

    arguments

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-# Loop through arguments, starting from index 1 to skip the script name
+# Print the arguments passed to the script
 for i in range(1, len(sys.argv)):
     print(sys.argv[i])
-
